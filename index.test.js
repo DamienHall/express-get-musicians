63e082e7b5558c519acd4f0169530b9343b155e7
DamienHall <damien.z.hall@gmail.com>
2025-03-11 14:46:56 -0500

    added testing for endpoints

--- index.test.js
+++ index.test.js
@@ -11,17 +11,44 @@ const {seedMusician} = require("./seedData");
 
 
 describe('./musicians endpoint', () => {
-    test("Testing musicians endpoint", async () => {
+    // POST
+    test("Can create musician", async () => {
+        const response = await request(app).post("/musicians").send({
+            "name": "Damien",
+            "instrument": "keytar"
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe("Musician created!");
+    });
+
+    // PUT
+    test("Can update musician", async () => {
+        const response = await request(app).put("/musicians/1").send({
+            "name": "Caviera"
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe("Musician updated!");
+    });
+
+    // GET
+    test("Can get all musicians", async () => {
         const response = await request(app).get("/musicians");
         const data = JSON.parse(response.text);
         expect(response.statusCode).toBe(200);
-        expect(data[0].name).toBe("Mick Jagger");
+        expect(data[0].name).toBe("Caviera");
     });
 
-    test("Testing musicians endpoint with :id", async () => {
+    test("Can get musician by id", async () => {
         const response = await request(app).get("/musicians/1");
         const data = JSON.parse(response.text);
         expect(response.statusCode).toBe(200);
-        expect(data.name).toBe("Mick Jagger");
+        expect(data.name).toBe("Caviera");
+    });
+    
+    // DELETE
+    test("Can delete musician by id", async () => {
+        const response = await request(app).delete("/musicians/1");
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe("Musician destroyed!");
     });
 });
