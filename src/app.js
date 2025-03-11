1ed98371216cfe6004cf2642afc99dc5f974f968
DamienHall <damien.z.hall@gmail.com>
2025-03-11 14:20:46 -0500

    created POST, PUT and DELETE endpoints

--- src/app.js
+++ src/app.js
@@ -4,6 +4,21 @@ const { Musician } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded());
+// CRUD
+
+app.post("/musicians", async (req, res) => {
+    await Musician.create(req.body);
+    res.send("Musician created!");
+});
+
+app.put("/musicians/:id", async (req, res) => {
+    let musician = await Musician.findByPk(req.params.id);
+    await musician.update(req.body);
+    await musician.save();
+    res.send("Musician updated!");
+});
 
 app.get("/musicians", async (req, res) => {
     res.json(await Musician.findAll());
@@ -12,6 +27,12 @@ app.get("/musicians", async (req, res) => {
 app.get("/musicians/:id", async (req, res) => {
     let musician = await Musician.findByPk(req.params.id);
     res.json(musician);
+});
+
+app.delete("/musicians/:id", async (req, res) => {
+    let musician = await Musician.findByPk(req.params.id);
+    musician.destroy();
+    res.send("Musician destroyed!");
 });
 
 module.exports = app;
